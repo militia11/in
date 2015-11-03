e9e2145be194148209ea9921373a31fdf757505c
Mateusz Michniewski <michniewski@onet.pl>
2015-11-03 22:17:33 +0100

    WERSJA DWA na JEDEN3

---
+++ ServerApplication/src/libs/controllers/CReceiverMock.cpp
@@ -0,0 +1,57 @@
+#include "CReceiverMock.h"
+
+QTcpSocket *CReceiverMock::GetSocket() const {
+		return 0;
+}
+
+const QString CReceiverMock::GetLog() const {
+	return mLog;
+}
+
+void CReceiverMock::NewData() {
+  mLog += "->NewData";
+}
+
+void CReceiverMock::RouteData(char aData) {
+
+}
+
+void CReceiverMock::ServeReceivedMessage() {
+
+}
+
+bool CReceiverMock::HasMessageCorrectFormat(char *aMessage) {
+
+}
+
+void CReceiverMock::ServeReceivedFileData() {
+
+}
+
+uint16_t CReceiverMock::CalculateFileDataChecksum(QByteArray aData) {
+  return 0;
+}
+
+int32_t CReceiverMock::ByteArrayToInt(QByteArray aData) {
+return 0;
+}
+
+void CReceiverMock::Connect(QTcpSocket *aSocket) {
+mLog += "->Connect";
+}
+
+void CReceiverMock::ConnectSocketSignals() {
+
+}
+
+void CReceiverMock::Disconnected() {
+
+}
+
+int CReceiverMock::ConvertMessageArrayToInt() {
+return 0;
+}
+
+void CReceiverMock::ResponeToClient(QByteArray aData) {
+	mLog += "->ResponeToClient(" + aData + ")";
+}
