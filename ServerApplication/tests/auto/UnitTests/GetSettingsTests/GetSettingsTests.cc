16d42aa3647cf76bb9056a253c3b577ce60fab37
Mateusz Michniewski <michniewski@onet.pl>
2015-11-06 02:36:30 +0100

    dodanie server mock6

---
+++ ServerApplication/tests/auto/UnitTests/GetSettingsTests/GetSettingsTests.cc
@@ -0,0 +1,77 @@
+#include <QString>
+#include <QtTest>
+#include <QCoreApplication>
+
+#include "src/libs/controllers/CSettings.h"
+
+///@todo temp settings zrobic zapisywanie gdzie indziej
+class GetSettingsTests : public QObject
+{
+    Q_OBJECT
+
+  public:
+		GetSettingsTests();
+
+	private:
+		void SaveSettingsInConfigHelper(
+				QString aValue,
+														 QString aKey,
+														 QString aGroup);
+
+  private Q_SLOTS:
+		void TestGetServerPortNumber();
+		void TestGetConnectionStringSQLiteDriver();
+		void TestGetConnectionStringMySqlDriver();
+};
+
+GetSettingsTests::GetSettingsTests()
+{
+}
+
+void GetSettingsTests::SaveSettingsInConfigHelper(QString aValue, QString aKey, QString aGroup)
+{
+	QSettings vQSettings;
+	vQSettings.beginGroup(aGroup);
+	vQSettings.setValue(aKey, aValue);
+	vQSettings.endGroup();
+}
+
+void GetSettingsTests::TestGetServerPortNumber()
+{
+	CSettings vSettings;
+
+	int vExpectedPortNumber {4639};
+	SaveSettingsInConfigHelper(QString::number(vExpectedPortNumber), "port", "server");
+
+QCOMPARE(vSettings.GetPortNumber(), vExpectedPortNumber);
+}
+
+void GetSettingsTests::TestGetConnectionStringSQLiteDriver()
+{
+
+}
+
+void GetSettingsTests::TestGetConnectionStringMySqlDriver()
+{
+
+}
+
+	//qDebug()<<vSettings.GetConnectionString();
+	//QString vExpectedConnectionString("host=129.0.4.1;user=testUser;password=0000;database=testDb");
+  //QCOMPARE(vSettings.GetConnectionString(), vExpectedConnectionString);*/
+/*
+  void SettingsTests::TestConnectionStringSQLiteDriver() {
+    /*CDatabaseConnectionDialogWrapper vDialog;
+    vDialog.mDriver = "sqlite3";
+    vDialog.mDatabaseName = "testDbSqlite";
+    vDialog.SaveSettings();
+
+    QString vExpectedConnectionString("database=testDbSqlite");
+    CSettings vSettings;
+
+    QCOMPARE(vSettings.GetConnectionString(), vExpectedConnectionString);
+}
+*/
+QTEST_MAIN(GetSettingsTests)
+
+#include "GetSettingsTests.moc"
---
+++ ServerApplication/tests/auto/UnitTests/GetSettingsTests/GetSettingsTests.pro
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-11-05T15:24:50
+#
+#-------------------------------------------------
+include(../../../../../AutogeneratingEmendation.pri)
+include(../../../../../LinkingLibraries.pri)
+
+QT       += widgets sql testlib network core testlib
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = GetSettingsTests
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+INCLUDEPATH += ../../../../
+
+SOURCES += GetSettingsTests.cc
+DEFINES += SRCDIR=\\\"$$PWD/\\\"
+
+LIBS += -L../../../../src/libs/controllers -lcontrollers
+LIBS += -L../../../../src/libs/ui -lui
+LIBS += -L../../../../src/libs/dao -ldao
